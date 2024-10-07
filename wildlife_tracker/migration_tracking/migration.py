5e0895197b9a1070466c6e3a176a7799bc7309dd
Ryan <hwangr@bu.edu>
2024-10-07 10:05:46 -0400

    wildlife tracker

---
+++ wildlife_tracker/migration_tracking/migration.py
@@ -0,0 +1,23 @@
+from typing import Any
+
+class Migration:
+
+    def __init__(self, 
+                 migration_id: int, 
+                 current_date: str, 
+                 current_location: str, 
+                 migration_path: MigrationPath, 
+                 start_date: str, 
+                 status: str= "Scheduled"):
+        self.migration_id = migration_id
+        self.current_date = current_date 
+        self.current_location = current_location 
+        self.migration_path = migration_path, 
+        self.start_date = start_date 
+        self.status = status
+    
+    def get_migration_details(migration_id: int) -> dict[str, Any]:
+        pass
+
+    def update_migration_details(migration_id: int, **kwargs: Any) -> None:
+        pass
